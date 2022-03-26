121fb10cce1901eda6b75c3de2fb1a03cf8d56fe
cglang <adolphlanglang@163.com>
2022-03-26 14:18:43 +0800

    添加CreateApplication拓展

---
+++ src/Ddon.Core/Microsoft/Extensions/Hosting/HostApplicationExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Ddon.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace Microsoft.Extensions.Hosting
+{
+    public static class HostApplicationExtensions
+    {
+        public static IHostBuilder CreateApplication<TMoudle>(this IHostBuilder hostBuilder, Action<IServiceCollection> configureDelegate)
+            where TMoudle : Module, new()
+        {
+            hostBuilder.ConfigureServices((x, services) =>
+            {
+                configureDelegate(services);
+                var configuration = (IConfiguration)services.Single(p => p.ServiceType == typeof(IConfiguration)).ImplementationInstance!;
+                new TMoudle().Load(services, configuration);
+            });
+            return hostBuilder;
+        }
+    }
+}
