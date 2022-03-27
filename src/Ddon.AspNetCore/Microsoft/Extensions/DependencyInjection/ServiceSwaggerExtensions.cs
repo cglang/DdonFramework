8c4f29514d405cb33bfab9e4364244dc6765a72d
cglang <adolphlanglang@163.com>
2022-03-27 14:14:41 +0800

    移动Swagger拓展位置

--- src/Ddon.AspNetCore/Microsoft/Extensions/DependencyInjection/ServiceSwaggerExtensions.cs
+++ src/Ddon.AspNetCore/Microsoft/Extensions/DependencyInjection/ServiceSwaggerExtensions.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Filters;
-using System;
-using System.IO;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
