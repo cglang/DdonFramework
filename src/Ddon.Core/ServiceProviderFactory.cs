96831fba77c97b18ea9d3c50946da5214c8c928c
cglang <adolphlanglang@163.com>
2022-03-27 23:26:00 +0800

    添加ServiceProviderFactory

---
+++ src/Ddon.Core/ServiceProviderFactory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Ddon.Core
+{
+    public class ServiceProviderFactory
+    {
+        private IServiceProvider? serviceProvider;
+
+        private static ServiceProviderFactory? serviceProviderFactory;
+
+        private static object _lock = new();
+
+        private ServiceProviderFactory() { }
+
+        private static ServiceProviderFactory InitInstance()
+        {
+            if (serviceProviderFactory is not null) return serviceProviderFactory;
+            lock (_lock)
+            {
+                serviceProviderFactory ??= new ServiceProviderFactory();
+            }
+            return serviceProviderFactory;
+        }
+
+        public static void InitServiceProvider(IServiceProvider serviceProvider)
+        {
+            var obj = InitInstance();
+            obj.serviceProvider = serviceProvider;
+        }
+
+        public static IServiceProvider GetServiceProvider()
+        {
+            var obj = InitInstance();
+            if (obj.serviceProvider is null)
+                throw new InvalidOperationException("ServiceProvider 未初始化");
+            return obj.serviceProvider;
+        }
+    }
+}
