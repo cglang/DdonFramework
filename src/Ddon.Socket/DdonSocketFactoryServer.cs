9913de4584ec11c0a00686f61251127495046f14
cglang <adolphlanglang@163.com>
2022-04-10 13:27:48 +0800

    加入连接日志记录

--- src/Ddon.Socket/DdonSocketFactoryServer.cs
+++ src/Ddon.Socket/DdonSocketFactoryServer.cs
@@ -1,6 +1,8 @@
 ﻿using Ddon.ConvenientSocket.Exceptions;
 using Ddon.Core;
 using Ddon.Socket.Route;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System.Net;
 using System.Net.Sockets;
 
@@ -9,19 +11,23 @@ namespace Ddon.Socket
     public class DdonSocketFactoryServer<TDdonSocketRouteMapLoadBase> where TDdonSocketRouteMapLoadBase : DdonSocketRouteMapLoadBase, new()
     {
         private readonly TcpListener _listener;
+        private readonly IServiceProvider _serviceProvider;
+
+        private ILogger? Logger => _serviceProvider.GetService<ILogger<DdonSocketFactoryServer<TDdonSocketRouteMapLoadBase>>>();
 
         public static DdonSocketStorage SocketStorage => DdonSocketStorage.GetInstance();
 
-        private DdonSocketFactoryServer(string host, int post)
+        private DdonSocketFactoryServer(IServiceProvider serviceProvider, string host, int post)
         {
             _listener = new TcpListener(IPAddress.Parse(host), post);
+            _serviceProvider = serviceProvider;
         }
 
         public static DdonSocketFactoryServer<TDdonSocketRouteMapLoadBase> CreateServer(IServiceProvider serviceProvider, string host, int post)
         {
             ServiceProviderFactory.InitServiceProvider(serviceProvider);
             DdonSocketRouteMap.Init<TDdonSocketRouteMapLoadBase>();
-            return new DdonSocketFactoryServer<TDdonSocketRouteMapLoadBase>(host, post);
+            return new DdonSocketFactoryServer<TDdonSocketRouteMapLoadBase>(serviceProvider, host, post);
         }
 
         public void Start()
@@ -35,6 +41,7 @@ namespace Ddon.Socket
                     var client = _listener.AcceptTcpClient();
                     var connection = new DdonSocketConnectionCore(client);
                     SocketStorage.Add(connection);
+                    Logger?.LogInformation("客户端接入：{SocketId}", connection.SocketId);
 
                     Task.Run(async () =>
                     {
@@ -45,6 +52,7 @@ namespace Ddon.Socket
                         catch (DdonSocketDisconnectException ex)
                         {
                             SocketStorage.Remove(ex.SocketId);
+                            Logger?.LogInformation("客户端断开：{SocketId}", ex.SocketId);
                         }
                     });
                 }
