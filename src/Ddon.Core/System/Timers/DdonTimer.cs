d47747fc4f7f38d582f71e2bd4be489b82897e79
cglang <adolphlanglang@163.com>
2022-04-23 22:57:51 +0800

    添加Timer类

---
+++ src/Ddon.Core/System/Timers/DdonTimer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Timers;
+
+namespace Ddon.Core.System.Timers
+{
+    class DdonTimer : Timer
+    {
+        private const double DefaultInterval = 60000;
+
+        private bool isDate = false;
+
+        private bool isRepeat = false;
+
+        private DateTime? date;
+
+        private double repeatInterval;
+
+        public DdonTimer SetAction(Action action)
+        {
+            Elapsed += (_, _) =>
+            {
+                var now = DateTime.Now;
+                if (isDate)
+                {
+                    if (now >= date)
+                    {
+                        if (isRepeat)
+                        {
+                            var date_s = date.Value.AddMilliseconds(repeatInterval);
+                            if (now >= date_s)
+                            {
+                                date = date_s;
+                                action();
+                            }
+                        }
+                        else
+                        {
+                            action();
+                            Stop();
+                        }
+                    }
+                }
+                else
+                {
+                    action();
+                }
+            };
+
+            return this;
+        }
+
+        public DdonTimer SetInterval(TimeSpan interval)
+        {
+            Interval = interval.TotalMilliseconds;
+            return this;
+        }
+
+        public DdonTimer SetDateTime(DateTime dateTime, TimeSpan? interval = null)
+        {
+            isDate = true;
+            if (interval is null)
+            {
+                Interval = DefaultInterval;
+            }
+            else
+            {
+                Interval = DefaultInterval;
+                repeatInterval = interval.Value.TotalMilliseconds;
+                isRepeat = true;
+            }
+            date = dateTime;
+            return this;
+        }
+
+        public new void Start()
+        {
+            base.Start();
+        }
+    }
+}
