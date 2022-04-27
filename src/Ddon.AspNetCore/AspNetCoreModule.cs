4b97b9cc7eaf462bae8286fe931f53b94fc1b6c5
cglang <adolphlanglang@163.com>
2022-04-27 17:29:18 +0800

    优化Module

---
+++ src/Ddon.AspNetCore/AspNetCoreModule.cs
@@ -0,0 +1,31 @@
+﻿using Ddon.AspNetCore.Filters;
+using Ddon.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+
+namespace Ddon.AspNetCore
+{
+    public class AspNetCoreModule : Module
+    {
+        public override void Load(IServiceCollection services, IConfiguration configuration)
+        {
+            // 向容器当中添加一些功能组成WebApi，包含AddControllers、AddHttpContextAccessor、AppSrtings
+            services.AddControllers(options =>
+            {
+                options.Filters.Add(typeof(GlobalExceptionFilter));
+            });
+
+            services.AddHttpContextAccessor();
+
+            var cors = configuration.GetSection("Cors").GetChildren().Select(c => c.Value).ToArray();
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: "DefaultCors", builder =>
+                {
+                    builder.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins(cors);
+                });
+            });
+        }
+    }
+}
--- src/Ddon.Identity/IdentityModule.cs
+++ src/Ddon.Identity/IdentityModule.cs
@@ -1,15 +1,48 @@
 ﻿using Ddon.Cache;
 using Ddon.Core;
+using Ddon.Domain.UserInfo;
+using Ddon.Identity.Jwt;
+using Ddon.Identity.Manager;
+using Ddon.Identity.Options;
+using Ddon.Identity.Permission;
+using Ddon.Repositiry.EntityFrameworkCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace Ddon.Identity
 {
-    public class IdentityModule : Module
+    public class IdentityModule<TDbContext, TKey, TPermissions> : Module
+        where TDbContext : DbContext
+        where TKey : IEquatable<TKey>
+        where TPermissions : class, IPermissionDefinitionProvider
     {
         public override void Load(IServiceCollection services, IConfiguration configuration)
         {
             Load<CacheModule>(services, configuration);
+
+            services.AddTransient<UserInfoInitMiddleware<TKey>>();
+
+            services.AddTransient<IIdentityManager<TKey>, IdentityManager<TKey>>();
+
+            services.AddTransient<IRoleClaimRepository<TKey>, RoleClaimRepository<TDbContext, TKey>>();
+            services.AddTransient<IRoleRepository<TKey>, RoleRepository<TDbContext, TKey>>();
+            services.AddTransient<IUserClaimRepository<TKey>, UserClaimRepository<TDbContext, TKey>>();
+            services.AddTransient<IUserRepository<TKey>, UserRepository<TDbContext, TKey>>();
+            services.AddTransient<IUserRoleRepository<TKey>, UserRoleRepository<TDbContext, TKey>>();
+            services.AddTransient<ITenantRepository<TKey>, TenantRepository<TDbContext, TKey>>();
+
+            services.AddSingleton<ITokenTools<TKey>, TokenTools<TKey>>();
+
+            services.AddScoped<ICurrentUserInfoAccessor<TKey>, CurrentUserInfoAccessor<TKey>>();
+
+            var appSettings = configuration.GetSection(nameof(AppSettings)).Get<AppSettings>();
+            services.AddSingleton(appSettings);
+
+            services.AddTransient<Auth<TKey>>();
+
+            services.AddTransient<IPermissionDefinitionProvider, TPermissions>();
         }
     }
 }
