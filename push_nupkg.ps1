26cf990dc1313e94f2efe3d3ee4cd47f095d7df7
cglang <adolphlanglang@163.com>
2025-11-08 16:35:58 +0800

    添加可以通过状态判断是否执行中间件的通用管道

--- push_nupkg.ps1
+++ push_nupkg.ps1
@@ -5,7 +5,8 @@ $rootFolder = (Get-Item -Path "./" -Verbose).FullName
 
 # List of projects
 $projects = (
-"Ddon.DependencyInjection"
+    #"Ddon.DependencyInjection",
+    "Ddon.Pipeline"
 )
 
 [xml]$commonPropsXml = Get-Content(Join-Path $rootFolder "version.props")
@@ -15,6 +16,39 @@ foreach($project in $projects) {
     $releasePath = Join-Path $rootFolder ("src" + "/" + $project + "/bin/Release/")
     $projectFolder = Join-Path $rootFolder ("src" + "/" + $project + "/" + $project + ".csproj")
 
-    dotnet publish $projectFolder -p:Configuration=Release -p:SourceLinkCreate=true -t:pack
+    dotnet build $projectFolder -c Release 
+    #dotnet publish $projectFolder -p:Configuration=Release -p:SourceLinkCreate=true -t:pack -f netstandard2.0
+    #dotnet publish $projectFolder -p:Configuration=Release -p:SourceLinkCreate=true -t:pack
     dotnet nuget push ($releasePath + $project + "." + $version + ".nupkg") --api-key $apikey --source https://api.nuget.org/v3/index.json --skip-duplicate
 }
+
+#foreach($project in $projects) {
+#    $projectFolder = Join-Path $rootFolder ("src/$project/$project.csproj")
+#
+#    $projXml = [xml](Get-Content $projectFolder)
+#    $targetFrameworks = @()
+#    if ($projXml.Project.PropertyGroup.TargetFramework) {
+#        $targetFrameworks += $projXml.Project.PropertyGroup.TargetFramework
+#    }
+#    if ($projXml.Project.PropertyGroup.TargetFrameworks) {
+#        $targetFrameworks += $projXml.Project.PropertyGroup.TargetFrameworks -split ";"
+#    }
+#
+#    foreach($framework in $targetFrameworks) {
+#        Write-Host "Building $project for framework $framework..."
+#        dotnet build $projectFolder -c Release #-p:TargetFramework=$framework
+#
+#        Write-Host "Packing $project for framework $framework..."
+#        $nupkgsFolder = [System.IO.Path]::Combine($rootFolder, "src", $project, "nupkgs")
+#        #dotnet pack $projectFolder -c Release -p:TargetFramework=$framework -p:SourceLinkCreate=true -o $nupkgsFolder
+#        dotnet pack $projectFolder -c Release -p:SourceLinkCreate=true -o nupkgs
+#
+#        # ¿ò¼Ü¶ÀÁ¢ÃüÃû£¬·ÀÖ¹¸²¸Ç
+#        $nupkgPath = [System.IO.Path]::Combine($nupkgsFolder, "nupkgs/$project.$version.$framework.nupkg")
+#        if (Test-Path $nupkgPath) {
+#            #dotnet nuget push $nupkgPath --api-key $apikey --source https://api.nuget.org/v3/index.json --skip-duplicate
+#        } else {
+#            Write-Host "Warning: nupkg not found for $framework at $nupkgPath"
+#        }
+#    }
+#}
---
+++ src/Ddon.Pipeline/StatePipe/GeneralCustomStatePipe.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Ddon.Pipeline.StatePipe
+{
+
+    public class GeneralCustomStatePipe
+    {
+        public static IGeneralCustomPipeline<TContext> GeneralCustomPipeline<TContext>(params StatePipeMiddleware<TContext>[] middlewares) where TContext : new()
+        {
+            return GeneralCustomPipelineFactory<TContext>.CreatePipelineBuild().ConfigureMiddlewares(builder =>
+            {
+                foreach (var middleware in middlewares)
+                {
+                    builder.AddMiddleware(context =>
+                    {
+                        if (middleware?.DecideFunc(context) ?? false)
+                        {
+                            middleware?.LogicBeforeFunc(context);
+                            middleware?.LogicFunc(context);
+                            middleware?.LogicAfterFunc(context);
+                        }
+                    });
+                }
+            }).Build();
+        }
+    }
+
+    public class StatePipeMiddleware<TContext>
+    {
+        public StatePipeMiddleware(
+            Func<TContext, bool> decideFunc,
+            Action<TContext> logicFunc,
+            Action<TContext> logicBeforeFunc = null,
+            Action<TContext> logicAfterFunc = null)
+        {
+            DecideFunc = decideFunc;
+            LogicFunc = logicFunc;
+            LogicBeforeFunc = logicBeforeFunc;
+            LogicAfterFunc = logicAfterFunc;
+        }
+
+        public Func<TContext, bool> DecideFunc { get; set; }
+
+        public Action<TContext> LogicFunc { get; set; }
+
+        public Action<TContext> LogicBeforeFunc { get; set; }
+
+        public Action<TContext> LogicAfterFunc { get; set; }
+    }
+}
