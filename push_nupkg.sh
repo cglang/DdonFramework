93243621ca61e174642e6cd13bbf523a1fed8812
cglang <adolphlanglang@163.com>
2026-07-22 22:54:12 +0800

    新包发布版本

--- push_nupkg.sh
+++ push_nupkg.sh
@@ -9,28 +9,28 @@ rootFolder="$(pwd)"
 
 # List of projects
 projects=(
-#    "Ddon.Common"
-#    "Ddon.DependencyInjection"
-#    "Ddon.Pipeline"
-#    "Ddon.Workflow"
-#    "Ddon.Cache"
-#    "Ddon.Cache.Redis"
-#    "Ddon.Cache.Memory"
-#    "Ddon.Serial"
-#    "Ddon.Socket"
-#    "Ddon.EventBus"
-#    "Ddon.EventBus.Memory"
-#    "Ddon.VitrinPLC"
-#    "Ddon.Hosting"
-    "Ddon.Desktop.Avalonia"
+    "Ddon.Common"
+    "Ddon.DependencyInjection"
+    "Ddon.Pipeline"
+    "Ddon.Workflow"
+    "Ddon.Cache"
+    "Ddon.Cache.Redis"
+    "Ddon.Cache.Memory"
+    "Ddon.Serial"
+    "Ddon.Socket"
+    "Ddon.EventBus"
+    "Ddon.EventBus.Memory"
+    "Ddon.VitrinPLC"
     "Ddon.Hosting"
+    "Ddon.Desktop.Core"
+    "Ddon.Desktop.Avalonia"
 )
 
 # Read version from version.props
 version=$(grep -oP '(?<=<Version>).*?(?=</Version>)' "$rootFolder/version.props")
 
 if [ -z "$version" ]; then
-    echo "Version not found in version.props"
+    echo "version.props 文件中找不到版本：Version"
     exit 1
 fi
 
@@ -39,21 +39,21 @@ for project in "${projects[@]}"; do
     releasePath="$rootFolder/src/$project/bin/Release"
     projectFile="$rootFolder/src/$project/$project.csproj"
 
-    echo "Building $project..."
+    echo "构建 $project..."
 
     dotnet build "$projectFile" -c Release
 
     package="$releasePath/$project.$version.nupkg"
 
     if [ -f "$package" ]; then
-         echo "Publishing $package..."
+         echo "上传 $package 中..."
 
          dotnet nuget push "$package" \
              --api-key "$apikey" \
              --source "https://api.nuget.org/v3/index.json" \
              --skip-duplicate
     else
-        echo "Warning: package not found: $package"
+        echo "警告: 找不到: $package"
     fi
 
 done
